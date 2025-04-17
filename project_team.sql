aabbb2e077c2ed42fb463e36a070247fcec90e03
sondinh12 <nguyendinhson92005@gmail.com>
2025-04-17 13:01:16 +0700

    new sql

--- project_team.sql
+++ project_team.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 08, 2025 at 03:43 PM
+-- Generation Time: Apr 17, 2025 at 05:59 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.20
 
@@ -43,9 +43,8 @@ CREATE TABLE `cart` (
 INSERT INTO `cart` (`id_cart`, `user_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES
 (1, 1, 1, 2, '2025-03-19 21:25:11', '2025-04-04 20:41:24'),
 (3, 3, 3, 4, '2025-03-19 21:25:11', '2025-04-04 20:41:38'),
-(12, 2, 1, 12, '2025-04-06 20:42:39', '2025-04-07 00:15:42'),
-(13, 2, 2, 4, '2025-04-06 22:22:25', '2025-04-06 23:35:28'),
-(15, 2, 3, 2, '2025-04-07 00:13:55', '2025-04-07 00:14:04');
+(56, NULL, 2, 1, '2025-04-17 12:41:58', '2025-04-17 12:41:58'),
+(61, 4, 2, 3, '2025-04-17 12:54:55', '2025-04-17 12:54:55');
 
 -- --------------------------------------------------------
 
@@ -67,8 +66,7 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`id_category`, `category_name`, `created_at`, `updated_at`) VALUES
 (1, 'Áo thun', '2025-03-19 21:24:14', '2025-03-19 21:24:14'),
 (2, 'Quần jean', '2025-03-19 21:24:14', '2025-03-19 21:24:14'),
-(3, 'Giày thể thao', '2025-03-19 21:24:14', '2025-03-19 21:24:14'),
-(10, 'Acer', '2025-04-03 09:27:24', '2025-04-03 09:27:24');
+(3, 'Giày thể thao', '2025-03-19 21:24:14', '2025-03-19 21:24:14');
 
 -- --------------------------------------------------------
 
@@ -103,7 +101,7 @@ CREATE TABLE `orders` (
   `id_order` int NOT NULL,
   `id_user` int DEFAULT NULL,
   `total` double(10,2) NOT NULL,
-  `status` enum('pending','completed','cancelled') DEFAULT 'pending',
+  `status` enum('pending','confirmed','delivering','completed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'pending',
   `payment` enum('cod','credit_card','paypal') DEFAULT 'cod',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -116,7 +114,49 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`id_order`, `id_user`, `total`, `status`, `payment`, `created_at`, `updated_at`) VALUES
 (1, 1, 650000.00, 'completed', 'cod', '2025-03-19 21:25:19', '2025-03-19 21:25:19'),
 (2, 2, 350000.00, 'pending', 'credit_card', '2025-03-19 21:25:19', '2025-03-19 21:25:19'),
-(3, 3, 500000.00, 'cancelled', 'paypal', '2025-03-19 21:25:19', '2025-03-19 21:25:19');
+(3, 3, 500000.00, 'cancelled', 'paypal', '2025-03-19 21:25:19', '2025-03-19 21:25:19'),
+(4, 2, 300.00, 'pending', 'credit_card', '2025-04-10 05:42:10', '2025-04-10 05:42:10'),
+(5, 2, 300.00, 'pending', 'credit_card', '2025-04-10 05:46:51', '2025-04-10 05:46:51'),
+(6, 2, 300.00, 'pending', 'credit_card', '2025-04-10 05:49:22', '2025-04-10 05:49:22'),
+(7, 2, 300.00, 'pending', 'credit_card', '2025-04-10 05:49:26', '2025-04-10 05:49:26'),
+(8, 2, 300.00, 'pending', 'cod', '2025-04-10 05:50:12', '2025-04-10 05:50:12'),
+(9, 2, 300.00, 'pending', 'cod', '2025-04-10 12:52:04', '2025-04-10 12:52:04'),
+(10, 2, 300.00, 'pending', 'cod', '2025-04-10 12:53:43', '2025-04-10 12:53:43'),
+(11, 2, 300.00, 'pending', 'cod', '2025-04-10 12:54:05', '2025-04-10 12:54:05'),
+(12, 2, 300.00, 'pending', 'cod', '2025-04-10 12:54:11', '2025-04-10 12:54:11'),
+(13, 2, 300.00, 'pending', 'cod', '2025-04-10 12:55:34', '2025-04-10 12:55:34'),
+(14, 2, 300.00, 'pending', 'cod', '2025-04-10 12:55:49', '2025-04-10 12:55:49'),
+(15, 2, 300.00, 'delivering', 'cod', '2025-04-10 12:56:23', '2025-04-12 10:53:44'),
+(16, 2, 300.00, 'pending', 'cod', '2025-04-10 12:57:12', '2025-04-10 12:57:12'),
+(17, 2, 150.00, 'completed', 'credit_card', '2025-04-10 13:00:39', '2025-04-10 18:13:24'),
+(18, 2, 700.00, 'delivering', 'cod', '2025-04-12 10:03:23', '2025-04-12 10:18:45'),
+(19, 2, 300.00, 'delivering', 'cod', '2025-04-12 10:56:13', '2025-04-12 10:56:29'),
+(20, 2, 1000000.00, 'delivering', 'cod', '2025-04-12 12:56:05', '2025-04-14 10:36:03'),
+(21, 2, 600000.00, 'delivering', 'cod', '2025-04-14 10:36:45', '2025-04-14 10:37:03'),
+(22, 2, 150000.00, 'delivering', 'cod', '2025-04-14 10:40:39', '2025-04-14 10:40:48'),
+(23, 2, 150000.00, 'delivering', 'cod', '2025-04-14 10:42:27', '2025-04-14 10:42:38'),
+(24, 2, 150000.00, 'pending', 'cod', '2025-04-14 10:48:29', '2025-04-14 10:48:29'),
+(25, 2, 150000.00, 'pending', 'cod', '2025-04-14 11:54:20', '2025-04-14 11:54:20'),
+(26, 2, 350000.00, 'pending', 'cod', '2025-04-14 11:54:27', '2025-04-14 11:54:27'),
+(27, 2, 500000.00, 'pending', 'cod', '2025-04-15 09:34:30', '2025-04-15 09:34:30'),
+(28, 2, 350000.00, 'delivering', 'cod', '2025-04-15 09:34:36', '2025-04-15 09:47:44'),
+(29, 2, 150000.00, 'confirmed', 'cod', '2025-04-15 09:34:42', '2025-04-15 09:35:28'),
+(30, 2, 500000.00, 'pending', 'cod', '2025-04-15 09:55:24', '2025-04-15 09:55:24'),
+(31, 2, 500000.00, 'pending', 'cod', '2025-04-15 09:56:02', '2025-04-15 09:56:02'),
+(32, 2, 500000.00, 'pending', 'cod', '2025-04-15 09:56:38', '2025-04-15 09:56:38'),
+(33, 2, 350000.00, 'pending', 'cod', '2025-04-15 10:21:42', '2025-04-15 10:21:42'),
+(34, 2, 350000.00, 'delivering', 'cod', '2025-04-15 10:22:43', '2025-04-15 10:25:56'),
+(35, 2, 350000.00, 'delivering', 'cod', '2025-04-15 21:05:47', '2025-04-15 21:06:08'),
+(36, 2, 1000000.00, 'delivering', 'cod', '2025-04-15 21:05:55', '2025-04-15 21:07:17'),
+(37, 2, 150000.00, 'delivering', 'cod', '2025-04-15 21:06:01', '2025-04-15 21:06:59'),
+(38, 2, 500000.00, 'delivering', 'cod', '2025-04-15 21:07:54', '2025-04-15 21:08:31'),
+(39, 2, 150000.00, 'delivering', 'cod', '2025-04-15 21:07:59', '2025-04-15 21:09:51'),
+(40, 2, 350000.00, 'delivering', 'cod', '2025-04-15 21:08:03', '2025-04-15 21:12:03'),
+(41, 2, 150000.00, 'delivering', 'cod', '2025-04-15 21:20:51', '2025-04-15 21:21:16'),
+(42, 2, 500000.00, 'completed', 'cod', '2025-04-15 21:21:01', '2025-04-15 21:34:14'),
+(43, 2, 150000.00, 'delivering', 'cod', '2025-04-16 22:22:22', '2025-04-16 22:31:16'),
+(44, 2, 500000.00, 'pending', 'cod', '2025-04-16 22:44:08', '2025-04-16 22:44:08'),
+(45, 4, 700000.00, 'pending', 'cod', '2025-04-17 12:54:10', '2025-04-17 12:54:10');
 
 -- --------------------------------------------------------
 
@@ -138,8 +178,23 @@ CREATE TABLE `order_details` (
 
 INSERT INTO `order_details` (`id_order_detail`, `order_id`, `pro_id`, `quantity`, `created_at`) VALUES
 (1, 1, 1, 2, '2025-03-19 21:25:26'),
-(2, 2, 2, 1, '2025-03-19 21:25:26'),
-(3, 3, 3, 1, '2025-03-19 21:25:26');
+(3, 3, 3, 1, '2025-03-19 21:25:26'),
+(8, 19, 1, 2, '2025-04-12 10:56:13'),
+(9, 20, 3, 2, '2025-04-12 12:56:05'),
+(10, 21, 1, 4, '2025-04-14 10:36:45'),
+(11, 22, 1, 1, '2025-04-14 10:40:39'),
+(12, 23, 1, 1, '2025-04-14 10:42:27'),
+(17, 28, 2, 1, '2025-04-15 09:34:36'),
+(26, 35, 2, 1, '2025-04-15 21:05:47'),
+(27, 36, 3, 2, '2025-04-15 21:05:55'),
+(28, 37, 1, 1, '2025-04-15 21:06:01'),
+(29, 38, 3, 1, '2025-04-15 21:07:54'),
+(30, 39, 1, 1, '2025-04-15 21:07:59'),
+(31, 40, 2, 1, '2025-04-15 21:08:03'),
+(32, 41, 1, 1, '2025-04-15 21:20:51'),
+(33, 42, 3, 1, '2025-04-15 21:21:01'),
+(34, 43, 1, 1, '2025-04-16 22:22:22'),
+(36, 45, 2, 2, '2025-04-17 12:54:10');
 
 -- --------------------------------------------------------
 
@@ -165,9 +220,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id_product`, `product_name`, `product_img`, `price`, `quantity`, `description`, `category_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Áo thun nam', 'uploads/products/1743647534_ẢNH 5.png', 150000.00, 50, 'Áo thun chất cotton cao cấp', 1, 'active', '2025-04-03 09:32:14', '2025-04-03 09:32:14'),
-(2, 'Quần jean nam', 'uploads/products/1743647523_Screenshot 2023-12-03 103726.png', 350000.00, 30, 'Quần jean cao cấp, co giãn tốt', 2, 'active', '2025-04-03 09:32:03', '2025-04-03 09:32:03'),
-(3, 'Giày thể thao', 'uploads/products/1743087865_Screenshot 2023-12-03 103716.png', 500000.00, 20, 'Giày thể thao nhẹ, bền đẹp', 3, 'active', '2025-03-27 22:04:25', '2025-03-27 22:04:25'),
+(1, 'Áo thun nam', 'uploads/products/1743647534_ẢNH 5.png', 150000.00, 30, 'Áo thun chất cotton cao cấp', 1, 'active', '2025-04-03 09:32:14', '2025-04-16 22:31:25'),
+(2, 'Quần jean nam', 'uploads/products/1743647523_Screenshot 2023-12-03 103726.png', 350000.00, 25, 'Quần jean cao cấp, co giãn tốt', 2, 'active', '2025-04-03 09:32:03', '2025-04-15 21:21:29'),
+(3, 'Giày thể thao', 'uploads/products/1743087865_Screenshot 2023-12-03 103716.png', 500000.00, 50, 'Giày thể thao nhẹ, bền đẹp', 3, 'active', '2025-03-27 22:04:25', '2025-04-15 21:36:57'),
 (4, 'sss', 'uploads/products/1743087421_Screenshot 2023-11-19 205308.png', 89457.00, 1, 'ưkmkg', 1, 'inactive', '2025-03-27 21:57:46', '2025-03-27 21:57:46');
 
 -- --------------------------------------------------------
@@ -220,7 +275,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id_user`, `name`, `user_name`, `user_img`, `email`, `phone`, `address`, `password`, `status`, `role`, `created_at`, `updated_at`) VALUES
 (1, 'Nguyễn Văn A', 'nguyenvana', 'user1.jpg', 'nguyenvana@example.com', '0987654321', 'Hà Nội', '482c811da5d5b4bc6d497ffa98491e38', 'active', 'user', '2025-03-19 21:24:51', '2025-03-19 21:24:51'),
 (2, 'Trần Thị B', 'tranthib', 'user2.jpg', 'tranthib@example.com', '0978123456', 'TP.HCM', '482c811da5d5b4bc6d497ffa98491e38', 'active', 'user', '2025-03-19 21:24:51', '2025-03-19 21:24:51'),
-(3, 'Admin', 'admin', 'admin.jpg', 'admin@example.com', '0909090909', 'Đà Nẵng', '0192023a7bbd73250516f069df18b500', 'active', 'admin', '2025-03-19 21:24:51', '2025-03-19 21:24:51');
+(3, 'Admin', 'admin', 'admin.jpg', 'admin@example.com', '0909090909', 'Đà Nẵng', '0192023a7bbd73250516f069df18b500', 'inactive', 'admin', '2025-03-19 21:24:51', '2025-04-17 12:56:58'),
+(4, 'sơn', 'sonplay', NULL, 'son@gmail.com', '087546544', 'Hà Nội', '$2y$10$4T2fB83xEzojrKQw0WR8wudnqZN6gQuwgekmDoT2NBc/YVeWfXZCS', 'active', 'admin', '2025-04-17 05:41:01', '2025-04-17 12:55:42');
 
 -- --------------------------------------------------------
 
@@ -325,7 +381,7 @@ ALTER TABLE `vouchers`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id_cart` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_cart` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -343,13 +399,13 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id_order_detail` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_order_detail` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -361,7 +417,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `vouchers`
